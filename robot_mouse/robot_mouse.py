f1f119f0558f3c9d888294ab88d33edff3f318f4
Ntobeko Malinga <ntmalin022@student.wethinkcode.co.za>
2022-10-14 11:08:59 +0200

    mines feature added

--- robot_mouse/robot_mouse.py
+++ robot_mouse/robot_mouse.py
@@ -45,8 +45,22 @@ def get_cheese(grid):
     return cheese_list
 
 
-def get_mines(grid):
-    pass
+def get_mines(grid, cheese_list):
+    mine_list = []
+    while len(mine_list) < 5:
+        coord = (randint(1, len(grid)-2), randint(1, len(grid)-2))
+        
+        for _ in grid:
+            if coord not in mine_list and coord not in cheese_list:
+                mine_list.append(coord)
+    return mine_list
+
+
+def place_remove_mine(grid, mine_position, place=True):
+    if place:
+        grid[mine_position[0][0]][mine_position[0][1]] = '💥'
+    else:
+        grid[mine_position[0][0]][mine_position[0][1]] = '🧱'
 
 
 def display_grid(grid: list, health=5, cheese_score=5):
@@ -115,7 +129,7 @@ def change_mouse_position(grid, old_mouse_position, new_mouse_position):
     grid[new_mouse_position[0][0]][new_mouse_position[0][1]] = '🐁'
 
 
-def update_mouse_info(grid, old_mouse_position, mouse_position, direction, cheese, cheese_score, health, mines=None, reward=None):
+def update_mouse_info(grid, old_mouse_position, mouse_position, direction, cheese, cheese_score, health, mines, reward=None):
     while old_mouse_position != mouse_position:
         prev_old_mouse_position = [old_mouse_position[0].copy()]
         if direction == 'u':
@@ -130,10 +144,21 @@ def update_mouse_info(grid, old_mouse_position, mouse_position, direction, chees
         if tuple(old_mouse_position[0]) in cheese:
             cheese_score += 1
             cheese.remove(tuple(old_mouse_position[0]))
+
+        if tuple(old_mouse_position[0]) in mines:
+            health -= 1
+            mouse_position = [prev_old_mouse_position[0].copy()]
+            mines.remove(tuple(old_mouse_position[0]))
+            place_remove_mine(grid, old_mouse_position)
+            display_grid(grid, health, cheese_score)
+            time.sleep(0.5)
+            place_remove_mine(grid, old_mouse_position, place=False)
+            display_grid(grid, health, cheese_score)
+            break
         change_mouse_position(grid, prev_old_mouse_position, old_mouse_position)
         display_grid(grid, health, cheese_score)
         time.sleep(0.3)
-    return cheese_score, cheese
+    return cheese_score, health, mouse_position
 
 
 def check_win_status(cheese):
@@ -143,10 +168,18 @@ def check_win_status(cheese):
     return True
 
 
+def check_lose_status(mines):
+    if len(mines) == 0:
+        print("Sorry, you lost!")
+        return False
+    return True
+
+
 if __name__ == '__main__':
     grid_size = get_grid_size()
     grid = make_grid(grid_size)
     cheese = get_cheese(grid)
+    mines = get_mines(grid, cheese)
     mouse_position = get_initial_mouse_position(grid)
     health = 5
     cheese_score = 0
@@ -154,9 +187,9 @@ if __name__ == '__main__':
     initialize_grid(grid, cheese, mouse_position)
     
     display_grid(grid, health, cheese_score)
-    while check_win_status(cheese):
+    while check_win_status(cheese) and check_lose_status(mines):
         old_mouse_position, mouse_position, direction = get_new_mouse_position(grid, mouse_position)
-        cheese_score, cheese = update_mouse_info(
+        cheese_score, health, mouse_position = update_mouse_info(
             grid, 
             old_mouse_position, 
             mouse_position, 
@@ -164,6 +197,6 @@ if __name__ == '__main__':
             cheese, 
             cheese_score, 
             health, 
-            mines=None, 
+            mines, 
             reward=None
         )
